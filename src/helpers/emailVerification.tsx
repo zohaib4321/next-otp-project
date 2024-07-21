def481591200d107241990d5cb3eefcc45d287c5
Zohaib <zohaib4321@gmail.com>
2024-07-21 09:03:21 +0500

    created the otp sending functionality using resend

---
+++ src/helpers/emailVerification.tsx
@@ -0,0 +1,30 @@
+import { Resend } from 'resend';
+import {ApiResponse} from "@/interfaces/ApiResponse"
+import VerificationEmail from "../../emails/VerificationEmail"
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendVerificationEmail(
+  username: string,
+  email: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: email,
+      subject: 'Mystry Message | Verification Code',
+      react: VerificationEmail({username, otp: verifyCode}),
+    });
+    return {
+      success: true, 
+      message: "Verification email sent"
+    }
+  } catch (emailError) {
+    console.error("Failed to sent verification email", emailError);
+    return {
+      success: false, 
+      message: "Failed to sent verification email "
+    }
+  }
+}
---
+++ src/model/User.ts
@@ -0,0 +1,69 @@
+import mongoose, {Schema, Document} from "mongoose"
+
+export interface Message extends Document {
+  content: string,
+  createdAt: Date
+}
+
+export interface User extends Document {
+  username: string,
+  email: string,
+  password: string,
+  verifyCode: string,
+  verifyCodeExpiry: Date,
+  isVerified: boolean,
+  isAcceptingMessage: boolean,
+  message: Message[]
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+})
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/, "please use a valid email address"]
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"]
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "VerifyCode is required"]
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "VerifyCodeExpiry is required"]
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true
+  },
+  message: [MessageSchema]
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
