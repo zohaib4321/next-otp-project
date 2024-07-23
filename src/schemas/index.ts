dbc3eecd0471d3e6720b01eba0549a4e0fddda01
Zohaib <zohaib4321@gmail.com>
2024-07-23 22:18:48 +0500

    Create endpoint for checking username availability

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,1 +1,7 @@
 import {z} from "zod"
+
+export const UsernameSchemaValidation = z
+  .string()
+  .min(3, "Username must be at least 3 characters")
+  .max(20, "Username must not be greater than 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters")
